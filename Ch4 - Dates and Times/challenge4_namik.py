0c35ec12e15e8a7476e0cb458d66c33aba62f659
Namik Mehmeti <namikmehmeti@gmail.com>
2022-01-12 00:16:58 +0200

    End of first module

--- Ch4 - Dates and Times/challenge4_namik.py
+++ Ch4 - Dates and Times/challenge4_namik.py
@@ -1,20 +1,36 @@
-# https://docs.python.org/3/library/calendar.html#calendar.monthcalendar
+import calendar
 
-# Enter year: f
-# invalid literal for int() with base 10: 'f'
-# Sorry, that's not valid input
 
-# Which day of the week do you want to count?
-# 0: Monday
-# 1: Tuesday
-# 2: Wednesday
-# 3: Thursday
-# 4: Friday
-# 5: Saturday
-# 6: Sunday
-# Or 'exit' to quit
-# ? 4
-# Enter year: 2022
-# Enter month: 1
-# There are 4 of those days in the month and year specified
-# -----------
+def getInput(prmpt, str, fin):
+    while(True):
+        try:
+            value = input(prmpt)
+            if value.lower() == 'exit':
+                exit()
+            if int(value) in range(str, fin+1):
+                return int(value)
+            else:
+                raise ValueError
+        except ValueError:
+            print("That's not valid input, try again... \n")
+
+
+def noDaysInMonYr(d, m, y):
+    givenDay = 0
+    for week in calendar.monthcalendar(y, m):
+        if week[d] != 0:
+            givenDay += 1
+    return givenDay
+
+
+print("--Day counter program--")
+prompt = "Which day of the week do you want to count?\n" + \
+    "0: Monday\n1: Tuesday\n2: Wednesday\n3: Thursday\n" + \
+    "4: Friday\n5: Saturday\n6: Sunday\nOr 'exit' to quit\n? "
+while(True):
+    inDay = getInput(prompt, 0, 6)
+    inYr = getInput("Enter year [0-3000]: ", 0, 3000)
+    inMon = getInput("Enter month [1-12]: ", 1, 12)
+    print("There are {} days of {} in the {} of {}.".format(
+        noDaysInMonYr(inDay, inMon, inYr), calendar.day_name[inDay], calendar.month_name[inMon], inYr))
+    print("-----------\n")
